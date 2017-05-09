7c0871fcb57dcc397f8a74cf38830e2b678dc442
Andrew Glines and David Rolfs <andrew+david@>
2017-05-09 10:57:48 -0700

    create branch that comes up with movie price

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,23 @@ function Ticket(movie, time, age){
   this.time = time;
   this.age = age;
 }
+function calculatePrice(age, time){
+  var price = 10;
+  if ( time < 2 ) {
+    if (age > 60 ) {
+      price -= 4;
+    }
+  }
+  else {
+    if ( time >= 2 ) {
+      if ( age > 60 ) {
+        price -= 2;
+      }
+    }
+  };
+  return price ;
+}; // END FUNCTION CALCPRICE
+
 
 
 
@@ -10,15 +27,16 @@ $(document).ready(function(){
   $("#movieForm").submit(function(event){
     event.preventDefault();
 
-    var movie = $("#movie1").val();
+    var movie = $("#movie").val();
     var time = $("#time").val();
     var age = parseInt($("#age").val());
 
      var newTicket = new Ticket(movie, time, age)
 
+      console.log(calculatePrice(newTicket.age, newTicket.time));
+
      console.log(newTicket)
-
-
+     console.log("movie ", newTicket.movie);
 
 
   })
